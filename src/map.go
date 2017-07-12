9919659e7d07fb84554dcd310cd22986551cba6a
王聪奕 <360805958@qq.com>
2017-07-12 17:24:45 +0800

    增加 数组，切片 创建语法
    增加 数组，切片 创建语法

--- src/map.go
+++ src/map.go
@@ -51,7 +51,12 @@ func main(){
    
    
    ************************************************
-   
+   arr := [5]int{1, 2, 3, 4, 5}
+
+	slice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(arr)
+	fmt.Println(slice)
    
    
 }
