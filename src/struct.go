cf85fd9948ff649a74ece907837f4de1bf7ecf61
王聪奕 <360805958@qq.com>
2017-07-19 10:17:29 +0800

    Update struct.go

--- src/struct.go
+++ src/struct.go
@@ -1,6 +1,30 @@
 //  由于go语言没有没有class的概念
 //  结构struct承担了面向对象的功能
 
+type vertex struct {
+	x, y int
+}
+
+var (
+	v1 = vertex{1, 2}
+	v2 = vertex{x:1}
+	v3 = vertex{}
+	v4 = &vertex{1, 2}
+)
+
+func main() {
+  fmt.Println(v1)                  //  {1,2}
+	fmt.Println(v2)                  //  {1,0}
+	fmt.Println(v3)                  //  {0,0}
+	fmt.Println(v4)                  //  &{1,2}    特殊的前缀 & 返回一个指向结构体的指针。
+}
+
+
+
+*********************************************
+*********************************************
+
+
 type human struct {
   Sex  int
 }
